438834a2bd79e03ddb4eb4b279cfd112eb40d295
Sir_Dirtsalot164 <sirdirtsalot@gmail.com>
2025-09-04 19:14:37 -0700

    code works now for graph

--- csePractice/graph/List.c
+++ csePractice/graph/List.c
@@ -1,113 +1,91 @@
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "List.h"
 
-#ifndef LIST_C
-#define LIST_C
-
-Node *newNode(void* data){
-    Node* new_node = malloc(sizeof(Node));
-    if (!new_node) {
-        fprintf(stderr, "malloc for node failed\n");
-        exit(1);
-    }
-    new_node->data = data;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    return new_node;
+Node* newNode(void* data) {
+        Node* new_node = malloc(sizeof(Node));
+        if (!new_node) exit(1);
+        new_node->data = data;
+        new_node->prev = NULL;
+        new_node->next = NULL;
+        return new_node;
 }
 
-void freeNode(Node* node){
-    if (node) {
-        if (node->data) free(node->data);
-        free(node);
-    }
+void freeNode(Node* node) {
+        if (node) {
+                free(node->data);
+                free(node);
+        }
 }
 
-List* newList(void){
-    List* L = malloc(sizeof(List));
-    if (!L) {
-        fprintf(stderr, "malloc for list failed\n");
-        exit(1);
-    }
-    L->length = 0;
-    L->index = -1;
-    L->front = NULL;
-    L->back = NULL;
-    L->cursor = NULL;
-    return L;
+List* newList(void) {
+        List* L = malloc(sizeof(List));
+        if (!L) exit(1);
+        L->length = 0;
+        L->index = -1;
+        L->front = NULL;
+        L->back = NULL;
+        L->cursor = NULL;
+        return L;
 }
 
-void freeList(List* L){
-    if (!L) return;
-    Node* cur = L->front;
-    while (cur) {
-        Node* next = cur->next;
-        freeNode(cur);
-        cur = next;
-    }
-    free(L);
+void freeList(List* L) {
+        if (L) {
+                Node* cur = L->front;
+                while (cur) {
+                        Node* tmp = cur->next;
+                        freeNode(cur);
+                        cur = tmp;
+                }
+                free(L);
+        }
 }
 
 int getLength(List* L) {
-    return L->length;
+        return L->length;
 }
 
-int getIndex(List* L){
-    if (L->cursor) return L->index;
-    else return -1;
+int getIndex(List* L) {
+        return L->index;
 }
 
-void* getFront(List* L){
-    return L->front->data;
+void* getFront(List* L) {
+        return L->front ? L->front->data : NULL;
 }
 
-void* getBack(List* L){
-    return L->back->data;
+void* getBack(List* L) {
+        return L->back ? L->back->data : NULL;
 }
 
-void* getCursor(List* L){
-    if (!L->cursor) {
-        fprintf(stderr, "getCursor called when cursor is undefined\n");
-        return NULL;
-    }
-    return L->cursor->data;
+void* getCursor(List* L) {
+        return L->cursor ? L->cursor->data : NULL;
 }
 
-void moveFront(List* L){
-    if (L->length > 0){
-        L->cursor = L->front;
-        L->index = 0;
-    }
+void moveFront(List* L) {
+        if (L->length > 0) {
+                L->cursor = L->front;
+                L->index = 0;
+        }
 }
 
-void moveBack(List* L){
-    if (L->length > 0){
-        L->cursor = L->back;
-        L->index = L->length - 1;
-    }
+void moveBack(List* L) {
+        if (L->length > 0) {
+                L->cursor = L->back;
+                L->index = L->length - 1;
+        }
 }
 
-void moveNext(List* L){
-    if (!L->cursor || L->index == L->length-1 || L->index == -1){
-        L->cursor = NULL;
-        L->index = -1;
-    } else {
-        L->cursor = L->cursor->next;
-        L->index++;
-    }
+void moveNext(List* L) {
+        if (L->cursor) {
+                L->cursor = L->cursor->next;
+                if (L->cursor) L->index++;
+                else L->index = -1;
+        }
 }
 
-void movePrev(List* L){
-    if (!L->cursor || L->index == 0 || L->index == -1){
-        L->cursor = NULL;
-        L->index = -1;
-    } else {
-        L->cursor = L->cursor->prev;
-        L->index--;
-    }
+void movePrev(List* L) {
+        if (L->cursor) {
+                L->cursor = L->cursor->prev;
+                if (L->cursor) L->index--;
+                else L->index = -1;
+        }
 }
-
-#endif
 
--- csePractice/graph/ListTest.c
+++ csePractice/graph/ListTest.c
@@ -1,56 +1,51 @@
-#include <stdlib.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "List.h"
 
-int main(void){
-    int *x1 = malloc(sizeof(int));
-    int *x2 = malloc(sizeof(int));
-    int *x3 = malloc(sizeof(int));
-    *x1 = 1;
-    *x2 = 2;
-    *x3 = 3;
+int main(void) {
+        int* x1 = malloc(sizeof(int));
+        int* x2 = malloc(sizeof(int));
+        int* x3 = malloc(sizeof(int));
+        *x1 = 10; *x2 = 20; *x3 = 30;
 
-    Node* a = newNode(x1);
-    Node* b = newNode(x2);
-    Node* c = newNode(x3);
+        Node* a = newNode(x1);
+        Node* b = newNode(x2);
+        Node* c = newNode(x3);
 
-    a->next = b;
-    b->next = c;
-    c->next = NULL;
-    a->prev = NULL;
-    c->prev = b;
-    b->prev = a;
+        a->next = b;
+        b->next = c;
+        c->next = NULL;
+        a->prev = NULL;
+        c->prev = b;
+        b->prev = a;
 
-    List* L = newList();
-    L->front = a;
-    L->back = c;
-    L->length = 3;
-    moveFront(L);
-    L->index = 0;
+        List* L = newList();
+        L->front = a;
+        L->back = c;
+        L->length = 3;
 
-    printf("%d\n",getIndex(L));
-    printf("%d\n",getLength(L));
-    printf("%d\n",*(int*)getFront(L));
-    printf("%d\n",*(int*)getBack(L));
-    printf("next\n");
-    for (int i = 0; i < 6; i++){
-        printf("E");
-        void* cur = getCursor(L);
-        if (cur) printf("%d: %d,\n", getIndex(L),*(int*)cur);
-        else printf("%d: (null),\n", getIndex(L));
-        moveNext(L);
-    }
-    printf("prvev\n");
-    moveBack(L);
-    for (int i = 0; i < 6; i++){
-        printf("E");
-        void* cur = getCursor(L);
-        if (cur) printf("%d: %d,\n", getIndex(L),*(int*)cur);
-        else printf("%d: (null),\n", getIndex(L));
-        movePrev(L);
-    }
+        moveFront(L);
+        printf("%d\n", getIndex(L));
+        printf("%d\n", getLength(L));
+        printf("%d\n", *(int*)getFront(L));
+        printf("%d\n", *(int*)getBack(L));
 
-    return 0;
+        for (int i = 0; i < getLength(L); i++) {
+        	void* cur = getCursor(L);
+                if (cur) printf("%d\n", *(int*)cur);
+                moveNext(L);
+        }
+
+        moveBack(L);
+        printf("%d\n", *(int*)getCursor(L));
+	
+	int* array = malloc(5 * sizeof(int));
+	for (int i = 0; i < 5; i++) {
+                array[i] = i + 1;
+        }
+	Node* nums = newNode(array);
+
+        freeList(L);
+        return 0;
 }
 
